4caa88a716bc549f4a9979d444b8c4112d3bf866
amasko <alexey.masko@gmail.com>
2013-02-06 01:28:45 +0400

    one more change

---
+++ src/com/def/first/com/def/unsinc/Hello.java
@@ -0,0 +1,15 @@
+package com.def.first.com.def.unsinc;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 06.02.13
+ * Time: 1:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello {
+    public Hello() {
+        System.out.print("Hello");
+
+    }
+}
